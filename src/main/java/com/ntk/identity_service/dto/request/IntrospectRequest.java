9392c8ac3ee0ed36b1df122328ae7b459032b06d
keyntk78 <key.ntk@gmail.com>
2024-08-25 14:46:45 +0700

    create, sign and verify a JWT token

--- src/main/java/com/ntk/identity_service/dto/request/IntrospectRequest.java
+++ src/main/java/com/ntk/identity_service/dto/request/IntrospectRequest.java
@@ -1,4 +1,4 @@
-package com.ntk.identity_service.dto.response;
+package com.ntk.identity_service.dto.request;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -8,7 +8,6 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class AuthenticationResponse {
-    String accessToken;
-    boolean authenticated;
+public class IntrospectRequest {
+    String token;
 }
