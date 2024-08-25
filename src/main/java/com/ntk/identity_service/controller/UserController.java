c4a5db3de95bdb805e4f63a3fbdb8d9ca011907d
keyntk78 <key.ntk@gmail.com>
2024-08-25 13:09:37 +0700

    Using Lombok and Mapstruct

--- src/main/java/com/ntk/identity_service/controller/UserController.java
+++ src/main/java/com/ntk/identity_service/controller/UserController.java
@@ -3,51 +3,52 @@ package com.ntk.identity_service.controller;
 import com.ntk.identity_service.dto.request.UserCreationRequest;
 import com.ntk.identity_service.dto.request.UserUpdateRequest;
 import com.ntk.identity_service.dto.response.ApiResponse;
-import com.ntk.identity_service.entity.User;
+import com.ntk.identity_service.dto.response.UserResponse;
 import com.ntk.identity_service.service.UserService;
 import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserController {
-    private UserService _userService;
+    UserService userService;
 
-    public UserController(UserService userService) {
-        _userService = userService;
-    }
 
     @PostMapping
-    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request){
-        ApiResponse<User> apiResponse = new ApiResponse<>();
-        apiResponse.setResult(_userService.createUser(request));
+    ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request){
+        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.createUser(request));
 
         return apiResponse;
     }
 
-
     @GetMapping
-    ApiResponse<List<User>> getUsers(){
-        ApiResponse<List<User>> apiResponse = new ApiResponse<>();
-        apiResponse.setResult(_userService.getUsers());
+    ApiResponse<List<UserResponse>> getUsers(){
+        ApiResponse<List<UserResponse>> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.getUsers());
 
         return apiResponse;
     }
 
     @GetMapping("/{userId}")
-    ApiResponse<User> getUser(@PathVariable("userId") String userId){
-        ApiResponse<User> apiResponse = new ApiResponse<>();
-        apiResponse.setResult( _userService.getUser(userId));
+    ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId){
+        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.getUser(userId));
 
         return apiResponse;
     }
 
     @PutMapping("/{userId}")
-    ApiResponse<User> updateUser(@PathVariable String userId,@Valid @RequestBody UserUpdateRequest request){
-        ApiResponse<User> apiResponse = new ApiResponse<>();
-        apiResponse.setResult(_userService.updateUser(userId, request));
+    ApiResponse<UserResponse> updateUser(@PathVariable String userId,@Valid @RequestBody UserUpdateRequest request){
+        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.updateUser(userId, request));
 
         return apiResponse;
     }
@@ -55,7 +56,7 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     ApiResponse<String> deleteUser(@PathVariable String userId){
-        _userService.deleteUser(userId);
+        userService.deleteUser(userId);
 
         ApiResponse<String> apiResponse = new ApiResponse<>();
         apiResponse.setResult("User has been deleted");
--- src/main/java/com/ntk/identity_service/service/UserService.java
+++ src/main/java/com/ntk/identity_service/service/UserService.java
@@ -2,54 +2,56 @@ package com.ntk.identity_service.service;
 
 import com.ntk.identity_service.dto.request.UserCreationRequest;
 import com.ntk.identity_service.dto.request.UserUpdateRequest;
+import com.ntk.identity_service.dto.response.UserResponse;
 import com.ntk.identity_service.entity.User;
+import com.ntk.identity_service.exception.AppException;
+import com.ntk.identity_service.exception.ErrorCode;
+import com.ntk.identity_service.mapper.UserMapper;
 import com.ntk.identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserService {
-    private UserRepository _userRepository;
+   UserRepository userRepository;
+   UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
-        _userRepository = userRepository;
-    }
+    public UserResponse createUser(UserCreationRequest request){
+        if (userRepository.existsByUsername(request.getUsername()))
+            throw new AppException(ErrorCode.USER_EXISTED);
+        User user  = userMapper.toUser(request);
 
-    public User createUser(UserCreationRequest request){
-        User user = new User();
+        return  userMapper.toUserResponse(userRepository.save(user));
 
-        user.setUsername(request.getUsername());
-        user.setPassword(request.getPassword());
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
-        user.setDob(request.getDob());
-
-        return _userRepository.save(user);
     }
 
 
-    public User updateUser(String userId, UserUpdateRequest request) {
-        User user = getUser(userId);
+    public UserResponse updateUser(String userId, UserUpdateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
 
-        user.setPassword(request.getPassword());
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
-        user.setDob(request.getDob());
+        userMapper.updateUser(user, request);
 
-        return _userRepository.save(user);
+        return userMapper.toUserResponse(userRepository.save(user));
     }
 
-    public List<User> getUsers(){
-        return _userRepository.findAll();
+    public List<UserResponse> getUsers(){
+        return userRepository.findAll().stream()
+                .map(userMapper::toUserResponse).toList();
     }
 
     public void deleteUser(String userId){
-        _userRepository.deleteById(userId);
+        userRepository.deleteById(userId);
     }
 
-    public User getUser(String id){
-        return _userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+    public UserResponse getUser(String id){
+        return userMapper.toUserResponse(userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found")));
     }
 }
